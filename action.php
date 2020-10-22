ceefea5eb40bf6675be10739f809e9600a023962
MahamudulHasan181196 <72815887+MahamudulHasan181196@users.noreply.github.com>
2020-10-22 14:41:41 +0600

    get method implementation
    get method implementation

---
+++ action.php
@@ -0,0 +1,22 @@
+<html>
+
+<body>
+
+
+
+Welcome <?php echo $_REQUEST["firstname"]; ?><br>
+Your name is: <?php echo $_POST["firstname"]; ?><br>
+Your username is: <?php echo $_POST["username"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your dateofbirth is: <?php echo $_POST["birthday"]; ?><br>
+Your phone is: <?php echo $_POST["phone"]; ?><br>
+Your address is: <?php echo $_POST["address"]; ?><br>
+Your password is: <?php echo $_POST["password"]; ?><br>
+
+
+
+
+
+</body>
+
+</html>
